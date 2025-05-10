8cfe72987a5e0b74deb85fd6b277c6f8a2427b0d
Alex Nogueira <alex@devaction.com.br>
2025-05-10 10:12:34 -0300

    feat(auth): add Permission model and related files

---
+++ app/Models/Auth/Permission.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types = 1);
+
+namespace App\Models\Auth;
+
+use Database\Factories\Auth\PermissionFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
+/**
+ * @property string $id
+ * @property string $name
+ * @property string $description
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
+class Permission extends Model
+{
+    /** @use HasFactory<PermissionFactory> */
+    use HasFactory;
+    use HasUlids;
+}
---
+++ database/factories/Auth/PermissionFactory.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types = 1);
+
+namespace Database\Factories\Auth;
+
+use App\Models\Auth\Permission;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Permission>
+ */
+class PermissionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name'        => $this->faker->unique()->word(),
+            'description' => $this->faker->sentence(),
+        ];
+    }
+}
