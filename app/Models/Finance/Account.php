a912d3fd3d9ea0aa0b4578fe004644c7cb06659a
Alex Nogueira <alex@devaction.com.br>
2025-05-18 20:53:47 -0300

    feat(finance): add account model, factory, migration, and seeder

---
+++ app/Models/Finance/Account.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace App\Models\Finance;
+
+use Database\Factories\Finance\AccountFactory;
+use Illuminate\Database\Eloquent\Attributes\UseFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $id
+ * @property string $name
+ * @property string $description
+ * @property string $type
+ * @property string $currency
+ * @property string $balance
+ * @property string $created_at
+ * @property string $updated_at
+ * @property string $tenant_id
+ * @property string $created_by
+ */
+#[UseFactory(AccountFactory::class)]
+class Account extends Model
+{
+    use HasFactory;
+    use HasUlids;
+}
