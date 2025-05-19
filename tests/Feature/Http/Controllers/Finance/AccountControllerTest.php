b875d6bdf2e4735d790babb3c704ccb1eccf2905
Alex Nogueira <alex@devaction.com.br>
2025-05-18 21:51:10 -0300

    test(AccountController): add filter account test case

--- tests/Feature/Http/Controllers/Finance/AccountControllerTest.php
+++ tests/Feature/Http/Controllers/Finance/AccountControllerTest.php
@@ -5,6 +5,7 @@ use App\Models\{Finance\Account, Tenant, User};
 use function Pest\Laravel\{actingAs, getJson};
 
 beforeEach(function () {
+    global $user, $tenant;
     $tenant = Tenant::factory()->create();
     $user   = User::factory()->create([
         'tenant_id' => $tenant->id,
@@ -13,12 +14,47 @@ beforeEach(function () {
 });
 
 it('should be able to list accounts', function () {
-    $account = Account::factory(10)->create();
+    Account::factory(10)->create();
 
     $response = getJson(route('finance.accounts.index'));
 
     $response->assertOk()
         ->assertJsonCount(10, 'data')
+        ->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'id',
+                    'name',
+                    'description',
+                    'type',
+                    'currency',
+                    'balance',
+                    'created_at',
+                    'updated_at',
+                ],
+            ],
+        ]);
+});
+
+it('should be able to filter account', function () {
+    global $user;
+
+    Account::factory(10)->create([
+        'tenant_id' => $user->tenant_id,
+    ]);
+    Account::factory()->create([
+        'name'      => 'Account 1',
+        'tenant_id' => $user->tenant_id,
+    ]);
+
+    $response = getJson(route('finance.accounts.index', [
+        'filter' => [
+            'name' => 'Account 1',
+        ],
+    ]));
+
+    $response->assertOk()
+        ->assertJsonCount(1, 'data')
         ->assertJsonStructure([
             'data' => [
                 '*' => [
